ffd13e549f9a71fb7b9a78d65d9420e85824ab77
Zavodsk0y <zimniyvolk5@gmail.com>
2023-11-07 13:34:12 +0700

    Провели рефакторинг представлений, обновили файл urls.py

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
+from .views import *
 
 urlpatterns = [
-   path('snippets/', views.snippet_list),
-   path('snippets/<int:pk>/', views.snippet_detail),
+   path('snippets/', SnippetList.as_view()),
+   path('snippets/<int:pk>/', SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
