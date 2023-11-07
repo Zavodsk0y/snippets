14b67f65ae0c30453bd0d19717449e9dea3d3ff4
Zavodsk0y <zimniyvolk5@gmail.com>
2023-11-07 17:06:38 +0700

    Создали единую точку входа. Будем возвращать список пользователей и сниппетов с помощью reverse по именам, которые будут определены в urls.py

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
 from rest_framework import permissions
-from .permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
+from .permissions import IsOwnerOrReadOnly
 from .serializers import *
 
 
@@ -28,3 +31,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
