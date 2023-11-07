c9f55b24cc1a1ee7d81e3e711f8616a9435329c2
Zavodsk0y <zimniyvolk5@gmail.com>
2023-11-07 16:45:47 +0700

    Добавляем пользователя, создавшего сниппет. Реализуем для него сериализатор. Создаем представления для просмотра списка пользователей В модели добавляем метод save для сохранения "представления" HTML

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -5,8 +5,11 @@ from . import views
 from .views import *
 
 urlpatterns = [
-   path('snippets/', SnippetList.as_view()),
-   path('snippets/<int:pk>/', SnippetDetail.as_view()),
+   path('snippets/', views.SnippetList.as_view()),
+   path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+   path('users/', views.UserList.as_view()),
+   path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
+
 
 urlpatterns = format_suffix_patterns(urlpatterns)
